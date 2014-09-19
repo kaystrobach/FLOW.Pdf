172caaa4330c01547d1f021203ed33c760fce696
Kay Strobach <github@kay-strobach.de>
2014-09-19 12:21:12 +0200

    Update MPdfRenderer.php

--- Classes/KayStrobach/Pdf/ViewHelpers/Renderer/MPdfRenderer.php
+++ Classes/KayStrobach/Pdf/ViewHelpers/Renderer/MPdfRenderer.php
@@ -9,15 +9,19 @@ class MPdfRenderer extends AbstractRenderer {
 	 */
 	protected function initLibrary() {
 		if(!class_exists('mPDF', FALSE)) {
-			require_once(FLOW_PATH_PACKAGES . 'Libraries/mpdf/mpdf/mpdf.php');
+			$autoloadPath = FLOW_PATH_PACKAGES . 'Libraries/mpdf/mpdf/mpdf.php';
+			if(is_file($autoloadPath)) {
+				require_once($autoloadPath);
+			} else {
+				throw new \Exception('please add mpdf/mpdf to your composer.json and install it');
+			}
 		}
 	}
 
 	/**
 	 *
 	 */
-	protected function convert($html = '')
-	{
+	protected function convert($html = '') {
 		$mpdf=new \mPDF();
 		#$mpdf->debug = TRUE;
 		$mpdf->WriteHTML($html);
