ed07be3b0954571a24356b6d938e5a7a9beb5998
Kay Strobach <info@kay-strobach.de>
2015-06-06 18:04:40 +0200

    [TASK] fix problem with new pdf view

--- Classes/KayStrobach/Pdf/View/PdfTemplateView.php
+++ Classes/KayStrobach/Pdf/View/PdfTemplateView.php
@@ -1,13 +1,45 @@
 <?php
 namespace KayStrobach\Pdf\View;
 
-
-use TYPO3\Fluid\View\TemplateView;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Fluid\View\TemplateView;
 
-class PdfTemplateView extends TemplateView{
+class PdfTemplateView extends TemplateView {
 	/**
-	 * @var \KayStrobach\Pdf\ViewHelpers\Renderer\MPdfRenderer()
+	 * @var array
+	 */
+	protected $supportedOptions = array(
+		'templateRootPathPattern' => array('@packageResourcesPath/Private/Templates', 'Pattern to be resolved for "@templateRoot" in the other patterns. Following placeholders are supported: "@packageResourcesPath"', 'string'),
+		'partialRootPathPattern' => array('@packageResourcesPath/Private/Partials', 'Pattern to be resolved for "@partialRoot" in the other patterns. Following placeholders are supported: "@packageResourcesPath"', 'string'),
+		'layoutRootPathPattern' => array('@packageResourcesPath/Private/Layouts', 'Pattern to be resolved for "@layoutRoot" in the other patterns. Following placeholders are supported: "@packageResourcesPath"', 'string'),
+
+		'templateRootPaths' => array(NULL, 'Path(s) to the template root. If NULL, then $this->options["templateRootPathPattern"] will be used to determine the path', 'array'),
+		'partialRootPaths' => array(NULL, 'Path(s) to the partial root. If NULL, then $this->options["partialRootPathPattern"] will be used to determine the path', 'array'),
+		'layoutRootPaths' => array(NULL, 'Path(s) to the layout root. If NULL, then $this->options["layoutRootPathPattern"] will be used to determine the path', 'array'),
+
+		'templatePathAndFilenamePattern' => array('@templateRoot/@subpackage/@controller/@action.@format', 'File pattern for resolving the template file. Following placeholders are supported: "@templateRoot",  "@partialRoot", "@layoutRoot", "@subpackage", "@action", "@format"', 'string'),
+		'partialPathAndFilenamePattern' => array('@partialRoot/@subpackage/@partial.@format', 'Directory pattern for global partials. Following placeholders are supported: "@templateRoot",  "@partialRoot", "@layoutRoot", "@subpackage", "@partial", "@format"', 'string'),
+		'layoutPathAndFilenamePattern' => array('@layoutRoot/@layout.@format', 'File pattern for resolving the layout. Following placeholders are supported: "@templateRoot",  "@partialRoot", "@layoutRoot", "@subpackage", "@layout", "@format"', 'string'),
+
+		'templatePathAndFilename' => array(NULL, 'Path and filename of the template file. If set,  overrides the templatePathAndFilenamePattern', 'string'),
+		'layoutPathAndFilename' => array(NULL, 'Path and filename of the layout file. If set, overrides the layoutPathAndFilenamePattern', 'string'),
+
+		'debug'             => array(FALSE,           'debug or not',                'boolean'),
+		'disable'           => array(FALSE,           'disable PDF, output html',    'boolean'),
+		'filename'          => array('pdf-@time.pdf', 'filename for download',       'string'),
+		'papersize'         => array('A4',            'set the papersize',           'string'),
+		'orientation'       => array('portrait',      'set the orientation',         'string'),
+		'basepath'          => array('',              'set the basepath',            'string'),
+		'dpi'               => array(96,              'set the quality of the pdf',  'integer'),
+		'enableHtml5Parser' => array(TRUE,            'html5parser or not',          'boolean'),
+		'enableCssFloat'    => array(TRUE,            'css floating or not',         'boolean'),
+		'renderer'          => array('MPdf',          'define the pdf renderer',     'string'),
+
+);
+
+	/**
+	 * @Flow\Inject
+	 * @var \KayStrobach\Pdf\ViewHelpers\Renderer\MPdfRenderer
 	 */
 	protected $renderHelper;
 
@@ -16,6 +48,7 @@ class PdfTemplateView extends TemplateView{
 	 * @return string
 	 */
 	public function render($actionName = NULL) {
+
 		/** @var \TYPO3\Flow\Http\Response $response */
 		$response = $this->controllerContext->getResponse();
 
@@ -23,6 +56,7 @@ class PdfTemplateView extends TemplateView{
 
 		$response->setHeader('Content-Disposition', 'inline; filename="fname.pdf"');
 		$response->setHeader('Content-Type', 'application/pdf; name="fileName.pdf"');
-		return $this->renderHelper->render(parent::render());
+
+		return $this->renderHelper->render(parent::render($actionName));
 	}
 }
