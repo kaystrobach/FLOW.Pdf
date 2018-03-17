c132f3e84ca5cb3da12e7fc7fdb395fa363b30ea
Eduardo Garces Hernandez <eghernandez@ariesprodesign.de>
2018-03-17 16:01:09 +0100

    Upgrade for Neos.Flow 4. Correction

--- Classes/ViewHelpers/PdfViewHelper.php
+++ Classes/ViewHelpers/PdfViewHelper.php
@@ -2,54 +2,68 @@
 namespace KayStrobach\Pdf\ViewHelpers;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "SBS.LaPo".              *
+ * This script belongs to the TYPO3 Flow package "KayStrobach.Pdf".       *
  *                                                                        *
  *                                                                        */
 
 use KayStrobach\Pdf\Renderer\Factory;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\Exception\StopActionException;
-use Neos\Fluid\Core\ViewHelper\AbstractViewHelper;
+use Neos\FluidAdaptor\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use Neos\FluidAdaptor\Core\Rendering\FlowAwareRenderingContextInterface;
 
 class PdfViewHelper extends AbstractViewHelper{
 
-	/**
-	 * @var int
-	 */
-	protected $errorReporting = 0;
+    /**
+     * @var int
+     */
+    protected $errorReporting = 0;
+
+    function initializeArguments() {
+        $this->registerArgument('debug',               'boolean', 'debug or not',                0, 0);
+        $this->registerArgument('disable',             'boolean', 'disable PDF, output html',    0, 0);
+        $this->registerArgument('filename',            'string',  'filename for download',       0, 'pdf-' . time() . '.pdf');
+        $this->registerArgument('papersize',           'string',  'set the papersize',           0, 'A4');
+        $this->registerArgument('orientation',         'string',  'set the orientation',         0, 'portrait');
+        $this->registerArgument('basepath',            'string',  'set the basepath',            0, '');
+        $this->registerArgument('dpi',                 'integer', 'set the quality of the pdf',  0, '96');
+        $this->registerArgument('enableHtml5Parser',   'boolean', 'html5parser or not',          0, 1);
+        $this->registerArgument('enableCssFloat',      'boolean', 'css floating or not',         0, 1);
+        $this->registerArgument('renderer',             'string', 'define the pdf renderer',     0, 'MPdf');
+    }
 
     /**
-     * @throws \Neos\Fluid\Core\ViewHelper\Exception
+     * @param RenderingContextInterface $renderingContext
+     * @return void
      */
-	public function initializeArguments() {
-		$this->registerArgument('debug',               'boolean', 'debug or not',                0, 0);
-		$this->registerArgument('disable',             'boolean', 'disable PDF, output html',    0, 0);
-		$this->registerArgument('filename',            'string',  'filename for download',       0, 'pdf-' . time() . '.pdf');
-		$this->registerArgument('papersize',           'string',  'set the papersize',           0, 'A4');
-		$this->registerArgument('orientation',         'string',  'set the orientation',         0, 'portrait');
-		$this->registerArgument('basepath',            'string',  'set the basepath',            0, '');
-		$this->registerArgument('dpi',                 'integer', 'set the quality of the pdf',  0, '96');
-		$this->registerArgument('enableHtml5Parser',   'boolean', 'html5parser or not',          0, 1);
-		$this->registerArgument('enableCssFloat',      'boolean', 'css floating or not',         0, 1);
-		$this->registerArgument('renderer',             'string', 'define the pdf renderer',     0, 'MPdf');
-	}
+    public function setRenderingContext(RenderingContextInterface $renderingContext)
+    {
+        $this->renderingContext = $renderingContext;
+        $this->templateVariableContainer = $renderingContext->getVariableProvider();
+        $this->viewHelperVariableContainer = $renderingContext->getViewHelperVariableContainer();
+        if ($renderingContext instanceof FlowAwareRenderingContextInterface) {
+            $this->controllerContext = $renderingContext->getControllerContext();
+        }
+    }
 
-	/**
-	 *
-	 * @throws \Neos\Flow\Mvc\Exception\StopActionException
-	 * @return string the rendered string
-	 */
-	public function render() {
-		if($this->arguments['disable']) {
+    /**
+     *
+     * @throws \Neos\Flow\Mvc\Exception\StopActionException
+     * @return string the rendered string
+     */
+    public function render() {
+        if(!$this->arguments['disable']) {
+
+            $renderer = Factory::get($this->arguments['renderer']);
+
+            $renderer->init($this->arguments);
+            $renderer->render($this->renderChildren());
+        } else {
             return $this->renderChildren();
         }
 
-        $renderer = Factory::get($this->arguments['renderer']);
-
-        $renderer->init($this->arguments);
-        $renderer->render($this->renderChildren());
-
-		//use instead of exit ;)
-		throw new StopActionException();
-	}
+        //use instead of exit ;)
+        throw new StopActionException();
+    }
 }
